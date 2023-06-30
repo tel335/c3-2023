f0729dfafc619f4bd57891ad4565f09b60dc7536
EliasMartinezUSM <elias.martinezr@usm.cl>
2023-06-30 18:52:38 -0400

    Completado caso de uso en que no se encontraban ciudades

--- src/domain/cities/repository/worldCitiesRespository.js
+++ src/domain/cities/repository/worldCitiesRespository.js
@@ -6,9 +6,19 @@ exports.getAllCitiesRepository = () => {
 
 exports.searchCitiesByCountryName = (inputCountryName) => { //Modificar el algoritmo de busqueda para que sea case insensitive
     const result = []
+    let found = false
     worldCitiesDataset.forEach((cityObject) => {
-        if((inputCountryName.toLowerCase()) === (cityObject.country.toLowerCase())) result.push(cityObject)
+        if((inputCountryName.toLowerCase()) === (cityObject.country.toLowerCase())) {
+            result.push(cityObject)
+            found = true
+        }
     })
+
+    if (!found) { //Agregada funcionalidad que notifica si no se encontraron ciudades
+        return {
+            "message": "No se encontraron ciudades para el país ingresado"
+        }
+    }
     return result
 }
 
--- tests/routes/cities/getCitiesByCountryUseCase.test.js
+++ tests/routes/cities/getCitiesByCountryUseCase.test.js
@@ -1,18 +1,27 @@
 import request from 'supertest'
 import { server, app } from '../../../src/index'
 
-//Este test busca probar el caso de uso en que se busca por pais y verifica que independiente del string ingresado, debe haber una respuesta valida
+//Este test busca probar los casos de uso que tengan que ver con busqueda por pais
 describe('/api/cities/by_country/:country', () => {
     afterAll(() => {
         server.close()
     })
 
-    test('Deberia responder con codigo 200 y devolver un arreglo con resultados', async () => { 
+    test('Si se encuentran resultados, deberia responder con codigo 200 y devolver un arreglo con los resultados', async () => { 
         const validString = 'chile'
-        console.log('/api/cities/by_country/'.concat(validString))
         const response = await request(app.callback()).get('/api/cities/by_country/'.concat(validString))
         expect(response.status).toBe(200)
         expect(Array.isArray(response.body)).toBe(true)
         expect(response.body.length > 0).toBe(true)
+    })
+
+    test('Si no se encuentran resultados, deberia responder con codigo 200 y un objeto de formato especifico', async () => { 
+        const invalidCountry = 'chule'
+        const expObj = {
+            "message": "No se encontraron ciudades para el país ingresado"
+        }
+        const response = await request(app.callback()).get('/api/cities/by_country/'.concat(invalidCountry))
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(expObj)
     })
 })
